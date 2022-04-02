d9908a86b3c9a6da7a609150c2995d0cea25d614
MH-Miguel <miguel.agustin.martinezherrera@ciencias.unam.mx>
2022-04-01 23:21:10 -0600

    Mondrian actualizado

--- edd/src/Automata/Mondrian.java
+++ edd/src/Automata/Mondrian.java
@@ -78,14 +78,25 @@ public class Mondrian extends AC {
         int blanco;
         int rojo;
         int negro;
-        for (int i = 0; i < Maux2.length; i++) {
-			for (int j = 0; j < Maux2.length; j++) {
-                amarillo =0;
+        int amarillo1;
+        int azul1;
+        int blanco1;
+        int rojo1;
+        int negro1;
+        amarillo =0;
+                amarillo1 =0;
                 azul=0;
                 blanco=0;
                 rojo=0;
                 negro=0;
-                // Revisamos la vecindad de cada casilla.
+                azul1=0;
+                blanco1=0;
+                rojo1=0;
+                negro1=0;
+        for (int i = 0; i < Maux2.length; i++) {
+			for (int j = 0; j < Maux2.length; j++) {
+                
+                // Revisamos la vecindad de cada casilla. Se utiliza la vecindad de Moore.
                 for (int k = i - 1; k <= i + 1; k++) {
 					for (int l = j - 1; l <= j + 1; l++) {
 
@@ -112,32 +123,78 @@ public class Mondrian extends AC {
                     }
                 }
 
-                // Editar los nuevos valores de la matriz CopiaM. segun los valores de la pila.
+                // Editar los nuevos valores de la matriz CopiaM. segun los valores de la pila, y limitando
+                // el crecimiento de los colores. Se intentó que el automata fuera evolucionando de tal manera
+                // que se realizaran franjas diagonales de colores que se mueven de derecha a izquierda.
 
-    
-                if ( pila.pop() == 1  || Maux2[i][j] == 1 ) {
+                if(Maux2[i][j] == 1){
+                    if ( pila.pop() == 0 || pila.pop() == 1 ){
+                    CopiaM[i][j] = 1;
+                    }else{
+                        if(azul1<248){
+                           CopiaM[i][j] = 2;
+                            blanco1++;
+                            azul1--;
+                        }else{
+                            CopiaM[i][j] = 1;
+                        }
+                    }
+                }else if(Maux2[i][j] == 2){
+                    if ( pila.pop() == 1|| pila.pop() == 2 ){
                     CopiaM[i][j] = 2;
-                }else if (pila.pop() == 2 || Maux2[i][j] == 2) {
+                    }else{
+                        if(blanco1<248){
+                           CopiaM[i][j] = 3;
+                            negro1++;
+                            blanco1--;
+                        }else{
+                            CopiaM[i][j] = 2;
+                        }
+                    }
+                }else if(Maux2[i][j] == 3){
+                    if ( pila.pop() == 2|| pila.pop() == 3  ){
                     CopiaM[i][j] = 3;
-                }else if (pila.pop() == 3 || Maux2[i][j] == 3) {
+                    }else{
+                        if(negro1<248){
+                           CopiaM[i][j] = 4;
+                            rojo1++;
+                            negro1--;
+                        }else{
+                            CopiaM[i][j] = 3;
+                        }
+                    }
+                }else if(Maux2[i][j] == 4){
+                    if ( pila.pop() == 3 || pila.pop() == 4 ){
                     CopiaM[i][j] = 4;
-                }else if (pila.pop() == 0 || Maux2[i][j] == 0) {
-                    CopiaM[i][j] = 1;
-                }else if (pila.pop() == 4 || Maux2[i][j] == 4) {
+                    }else{
+                        if(rojo1<248){
+                           CopiaM[i][j] = 0; 
+                           amarillo1++;
+                           rojo1--;
+                        }else{
+                            CopiaM[i][j] = 4;
+                        }
+                        
+                    }
+                }else {
+                    if ( pila.pop() == 4 || pila.pop() == 0 ){
                     CopiaM[i][j] = 0;
-                }else if (Maux2[i][j] == 3 || Maux2[i][j] == 0 || Maux2[i][j] == 4 ) {
-                    CopiaM[i][j] = 1;
-                }
-                else {
-                    CopiaM[i][j] = Maux2[i][j];
+                    }else{
+                        if(amarillo1<248){
+                           CopiaM[i][j] = 1;
+                            azul1++;
+                            amarillo1--;
+                        }else{
+                            CopiaM[i][j] = 0;
+                        }
+                    }
                 }
             
-
-
             }
+            
 
+        
         }
-
         for (int i = 0; i < Maux2.length; i++) { // Fors que arreglan la matriz a regresar en la copia.
             for (int j = 0; j < Maux2.length; j++) {
                 Maux2[i][j] = CopiaM[i][j];
