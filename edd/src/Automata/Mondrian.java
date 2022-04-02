e7b75b0adea0d53a380364801b77879590ab817b
MH-Miguel <miguel.agustin.martinezherrera@ciencias.unam.mx>
2022-04-01 21:35:18 -0600

    automata deficiente xd

--- edd/src/Automata/Mondrian.java
+++ edd/src/Automata/Mondrian.java
@@ -62,9 +62,9 @@ public class Mondrian extends AC {
 
         //Creamos una pila y una cola para que te diviertas joven Artista. 
         Pila<Integer> pila = new Pila<Integer>();
-        pila.push(2);
-        pila.pop();
-        pila.push(pila.pop());
+        //pila.push(2);
+        //pila.pop();
+        //pila.push(pila.pop());
         Pila<Integer> cola = new Pila<Integer>();
         
         //La matriz Maux2 Contiene el estado actual de la matriz.
@@ -72,6 +72,71 @@ public class Mondrian extends AC {
         
         // Aqui va tu codigo  
 
+
+        int amarillo;
+        int azul;
+        int blanco;
+        int rojo;
+        int negro;
+        for (int i = 0; i < Maux2.length; i++) {
+			for (int j = 0; j < Maux2.length; j++) {
+                amarillo =0;
+                azul=0;
+                blanco=0;
+                rojo=0;
+                negro=0;
+                // Revisamos la vecindad de cada casilla.
+                for (int k = i - 1; k <= i + 1; k++) {
+					for (int l = j - 1; l <= j + 1; l++) {
+
+                        if (k >= 0 && l >= 0 && k < Maux2.length && l < Maux2.length && (k != i || l != j)) {
+                            if (Maux2[k][l] == 1) {
+                                pila.push(1);
+                                azul++;
+                            }else if(Maux2[k][l] == 2) {
+                                pila.push(2);
+                                blanco++;
+                            }else if(Maux2[k][l] == 3) {
+                                pila.push(3);
+                                negro++;
+                            }else if(Maux2[k][l] == 4) {
+                                pila.push(4);
+                                rojo++;
+                            }
+                            else {
+                                pila.push(0);
+                                amarillo++;
+                            }
+                        }
+
+                    }
+                }
+
+                // Editar los nuevos valores de la matriz CopiaM. segun los valores de la pila.
+
+    
+                if ( pila.pop() == 1  || Maux2[i][j] == 1 ) {
+                    CopiaM[i][j] = 2;
+                }else if (pila.pop() == 2 || Maux2[i][j] == 2) {
+                    CopiaM[i][j] = 3;
+                }else if (pila.pop() == 3 || Maux2[i][j] == 3) {
+                    CopiaM[i][j] = 4;
+                }else if (pila.pop() == 0 || Maux2[i][j] == 0) {
+                    CopiaM[i][j] = 1;
+                }else if (pila.pop() == 4 || Maux2[i][j] == 4) {
+                    CopiaM[i][j] = 0;
+                }else if (Maux2[i][j] == 3 || Maux2[i][j] == 0 || Maux2[i][j] == 4 ) {
+                    CopiaM[i][j] = 1;
+                }
+                else {
+                    CopiaM[i][j] = Maux2[i][j];
+                }
+            
+
+
+            }
+
+        }
 
         for (int i = 0; i < Maux2.length; i++) { // Fors que arreglan la matriz a regresar en la copia.
             for (int j = 0; j < Maux2.length; j++) {
